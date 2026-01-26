4c1ccb5a6d1179abacf3b82c7108f1083d0ddd5c
Bianca Correa <me@aquabianca.dev>
2026-01-26 20:23:54 -0300

    feat(categ): refactor categorizer to use model

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import src.models.category as category
+import src.structs.category as category
 import src.etl.categorizer as categorizer
 import src.etl.remainder as remainder
 import src.etl.ingestor as ingestor
@@ -17,19 +17,19 @@ def main():
 
     # For each category in the ledger:
     for cat in category.build_categories():
-        categ = categorizer.Categorizer(cat)
+        categ = categorizer.Categorizer(d, cat)
 
         # Initialize the table if needed
-        categ.create_table(db)
+        categ.create_table()
 
         # Get rows for the current category
-        res = categ.query(db)
+        res = categ.query()
 
         # Add categorized rows to the ledger
-        categ.persist(db, res)
+        categ.persist(res)
 
         # Export to csv
-        categ.export(db)
+        categ.export()
 
     # Calculate remainder table
     rem = remainder.Remainder()
--- src/etl/categorizer.py
+++ src/etl/categorizer.py
@@ -1,59 +1,17 @@
+from src.structs.runtime.duck import DuckRuntime
+from src.structs.model import Model
+from src.structs.category import Category
 import polars as pl
-import src.models.category as category
-import duckdb
 
 
-class Categorizer:
-    def __init__(self, category: category.Category) -> None:
+class Categorizer(Model):
+    def __init__(self, rt: DuckRuntime, category: Category) -> None:
+        super().__init__(kind="categorizer", rt=rt)
+
         self.category = category
 
-    def query(self, db):
-        statement = "SELECT * FROM spends.ledger_struct WHERE LOWER(tx_title) LIKE '{}%' ORDER BY tx_amount DESC".format(
-            self.category.prefix
-        )
-
-        return (
-            db.execute(
-                statement,
-            )
-            .pl()
-            .with_columns(pl.lit(self.category.name).alias("tx_category"))
-        )
-
-    def persist(self, db, df):
-        return db.execute(
-            """
-            MERGE INTO spends.categories
-                USING (SELECT * FROM df) as new
-                USING (
-                    id,
-                    tx_date,
-                    tx_amount,
-                    tx_title,
-                    tx_category
-                )
-                WHEN MATCHED THEN UPDATE
-                WHEN NOT MATCHED THEN INSERT;
-            """
-        )
-
-    def export(self, db: duckdb.DuckDBPyConnection):
-        df = db.execute(
-            """
-                SELECT * 
-                FROM spends.categories 
-                WHERE tx_category = ? AND LOWER(tx_title) LIKE '{}%';
-            """.format(
-                self.category.prefix
-            ),
-            [self.category.name],
-        ).pl()
-
-        return df.write_csv("data/{}.csv".format(self.category.id()))
-
-    def create_table(self, db):
-        return db.sql(
-            """
+    def create_table(self):
+        stmt = """
                 CREATE TABLE IF NOT EXISTS spends.categories (
                     id UBIGINT PRIMARY KEY,
                     tx_date DATE,
@@ -62,4 +20,38 @@ class Categorizer:
                     tx_category VARCHAR
                 );
             """
+
+        return self.rt.context.execute(stmt)
+
+    def read(self) -> str:
+        stmt = "SELECT * FROM spends.ledger_struct WHERE LOWER(tx_title) LIKE '{}%' ORDER BY tx_amount DESC".format(
+            self.category.prefix
         )
+
+        return stmt
+
+    def write(self) -> str:
+        return """
+            INSERT OR IGNORE INTO spends.categories (id, tx_date, tx_amount, tx_title, tx_category) 
+            SELECT id, tx_date, tx_amount, tx_title, tx_category FROM df;
+        """
+
+    def transform(self, df: pl.DataFrame) -> pl.DataFrame:
+        df = df.with_columns(pl.lit(self.category.name).alias("tx_category"))
+
+        return df
+
+    def export(self):
+        stmt = """
+                SELECT * 
+                FROM spends.categories 
+                WHERE tx_category = ? AND LOWER(tx_title) LIKE '{}%';
+            """.format(
+            self.category.prefix
+        )
+        df = self.rt.context.execute(
+            stmt,
+            [self.category.name],
+        ).pl()
+
+        return df.write_csv("data/{}.csv".format(self.category.id()))
