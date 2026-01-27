2676f4a1c99e6138a5b193b137be9bf242c8f13b
Bianca Correa <me@aquabianca.dev>
2026-01-27 13:08:30 -0300

    feat: declarative ledger

--- main.py
+++ main.py
@@ -3,21 +3,28 @@ import src.etl.remainder as remainder
 import src.etl.ingestor as ingestor
 import src.structs.category as category
 import src.structs.runtime.v2.duck as duck
+import src.structs.ledger as ledger
 
 
 def main():
-    d = duck.DuckRuntime()
-    models = list()
+    ldg = ledger.Ledger()
+    db_name = ldg.ledger["database"]["name"]
+    d = duck.DuckRuntime(db_name)
 
-    models.append(ingestor.Ingestor("2026-01"))
+    targets = ldg.ingestion["targets"]
+    for target in targets:
+        print("========= Processing {} =========".format(target))
+        models = list()
+        models.append(ingestor.Ingestor(target))
 
-    for cat in category.build_categories():
-        models.append(categorizer.Categorizer(cat))
+        for cat in category.build_categories(ldg.categories):
+            models.append(categorizer.Categorizer(cat))
 
-    models.append(remainder.Remainder())
+        models.append(remainder.Remainder())
 
-    for model in models:
-        d.run(model)
+        for model in models:
+            d.run(model)
+        print()
 
 
 if __name__ == "__main__":
---
+++ src/structs/ledger.py
@@ -0,0 +1,14 @@
+import yaml
+
+
+class Ledger:
+    """
+    Manages Ledger declarations such as input_files
+    """
+
+    def __init__(self, cfg: str = "ledger.yaml") -> None:
+        with open(cfg) as f:
+            self.ledger = yaml.safe_load(f)
+        self.database = self.ledger["database"]
+        self.ingestion = self.ledger["ingestion"]
+        self.categories = self.ledger["categories"]
